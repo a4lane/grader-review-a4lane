4899a919131410d245d861977d4f896a8e21b988
a4lane <a4lane@ucsd.edu>
2023-11-07 16:13:29 -0800

    commit

--- grade.sh
+++ grade.sh
@@ -8,9 +8,33 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ ! -f student-submission/ListExamples.java ]
+then
+    echo "File does not exist"
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > junit-results.txt
+
+
+result=$( tail -n 2 junit-results.txt )
+
+if [ $result == OK*]
+then
+    echo "GRADE: 100%"
+else 
+fi
+
+echo $result
